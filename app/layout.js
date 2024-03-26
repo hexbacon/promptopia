c91f8aff12252594e651d5d1c9b8c1c74ddc10db
Christopher Mena <you@chrismena.dev>
2024-03-26 14:29:36 -0400

    implemented login with google

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,24 @@
 import '@styles/global.css';
 import Nav from '@components/Nav';
-import  Provider  from '@components/Provider';
+import Provider from '@components/Provider';
 export const metadata = {
     title: "Promptopia",
     description: "Discover AI Prompt"
 }
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
     return (
         <html>
             <body>
-                <div className='main'>
-                    <div className='gradient' />
-                </div>
-                <main className='app'>
-                    <Nav />
-                    {children}
-                </main>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='app'>
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     )
