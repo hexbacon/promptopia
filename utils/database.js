aea8225be57e9d73c281e014e26d0451c7a998a6
Christopher Mena <you@chrismena.dev>
2024-03-26 16:17:36 -0400

    fixed issue with db and depracated options

--- utils/database.js
+++ utils/database.js
@@ -12,8 +12,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: 'share_prompts',
-            userNewUrlParser: true,
-            useUnifiedTopology: true,
         })
         isConnected = true;
         console.log("MongoDB is connected");
